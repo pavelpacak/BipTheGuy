5f610ea0f16cce48a33f3e9b5e15e8bf582d10f3
Pavel Pacák <pavel.pacak@gmail.com>
2025-04-30 18:40:38 +0200

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Pavel Pacák on 30.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
