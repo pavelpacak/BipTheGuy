d1832eb47982655571cc107badedbeba616133c6
Pavel Pacák <pavel.pacak@gmail.com>
2025-05-03 17:38:40 +0200

    Changes after lesson 3.3: PohotosPicker

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -7,16 +7,19 @@
 
 import SwiftUI
 import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
     @State private var isFullSize = true
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
     
     var body: some View {
         VStack {
             Spacer()
             
-            Image("clown")
+            bipImage
                 .resizable()
                 .scaledToFit()
                 .scaleEffect(isFullSize ? 1.0 : 0.9)
@@ -30,10 +33,21 @@ struct ContentView: View {
             
             Spacer()
             
-            Button {
-                // TODO: Button action here
-            } label: {
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
                 Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+            .onChange(of: selectedPhoto) {
+                Task {
+                    do {
+                        if let data = try await selectedPhoto?.loadTransferable(type: Data.self) {
+                            if let uiImage = UIImage(data: data) {
+                                bipImage = Image(uiImage: uiImage)
+                            }
+                        }
+                    } catch {
+                        print("😡 ERROR: loading failed \(error.localizedDescription)")
+                    }
+                }
             }
             
         }
